29187583da560e6ef441170a9b7726cb7a82e1af
o0o52lin <237277669@qq.com>
2023-09-19 11:04:41 +0800

    update

--- Applications/Tenyou/Timer/Tasktimer.php
+++ Applications/Tenyou/Timer/Tasktimer.php
@@ -38,15 +38,7 @@ class Tasktimer extends Base
 			}
 			foreach ($timers as $v){
 				$minid = $v['id'];
-				$ps = json_decode($v['params'],TRUE);
-				if(($ps['type'] ?? -1) === 0){
-					$this->call('Business\\Wool\\GrabNewest', array_merge($ps, array('taskid'=>$v['id']) ));
-				}else if(($ps['type'] ?? -1) > 1000){
-					$this->call('Business\\Wool\\GrabRank', array_merge($ps, array('taskid'=>$v['id']) ));
-				}else{
-					$this->call($v['type'], array_merge($ps, array('taskid'=>$v['id']) ));
-				}
-				
+				$this->call($v['type'], array_merge( json_decode($v['params'],TRUE), array('taskid'=>$v['id']) )); //把taskid数组放在后面，避免被设置参数覆盖
 			}
 			$j += 1;
 		}while ($j < 100);
